94ba92f20a665d431878cf85fba7a342ddfa9759
timur topaev <timur topaev>
2016-11-18 16:48:05 +0300

    first day

---
+++ app/src/main/java/v/challengeyourself/ProfileActivity.java
@@ -0,0 +1,18 @@
+package v.challengeyourself;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by AdminPC on 18.11.2016.
+ */
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+    }
+}

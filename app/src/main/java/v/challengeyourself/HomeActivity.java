a1d77845aa7f758fd386239803c7674bd5b2a591
topaevtimur <topaev-timur@mail.ru>
2016-11-25 18:23:53 +0300

    Add files via upload

--- app/src/main/java/v/challengeyourself/HomeActivity.java
+++ app/src/main/java/v/challengeyourself/HomeActivity.java
@@ -1,13 +1,32 @@
 package v.challengeyourself;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+
+    void save() {}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button profile_btn = (Button) findViewById(R.id.profile);
+        profile_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                save();
+                Intent intent = new Intent(HomeActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button calendar_btn = (Button) findViewById(R.id.calendar);
+        Button challenges_btn = (Button) findViewById(R.id.challenges);
+        Button editor_btn = (Button) findViewById(R.id.editor);
     }
 }

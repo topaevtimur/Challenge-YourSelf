27bb0c0e860f9937e756a188d98cdfd846b8b6ec
Stanislava <stasya.gera@yandex.ru>
2016-12-13 11:45:29 +0300

    calendar added

---
+++ app/src/main/java/v/challengeyourself/EditorActivity.java
@@ -0,0 +1,15 @@
+package v.challengeyourself;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by penguinni on 27.11.16.
+ */
+public class EditorActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+    }
+}

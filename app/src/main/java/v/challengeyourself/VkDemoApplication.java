980a0ce229039ba3d92bc231ca3c23f3222d2194
AdminPC <topaev-timur@mail.ru>
2016-12-27 17:03:22 +0300

    add recyclerView for done Challenges and authentication  VK

---
+++ app/src/main/java/v/challengeyourself/VkDemoApplication.java
@@ -0,0 +1,25 @@
+package v.challengeyourself;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.stetho.Stetho;
+import com.vk.sdk.VKSdk;
+
+/**
+ * Created by AdminPC on 20.12.2016.
+ */
+
+public class VkDemoApplication extends Application {
+
+   @Override
+    public void onCreate() {
+        Log.d("vk","VKKKKKKKKKKKKKKKKKKKKKKKKKKK");
+        super.onCreate();
+        Stetho.initializeWithDefaults(this);
+        Fresco.initialize(this);
+        VKSdk.initialize(this);
+    }
+
+}
